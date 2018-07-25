249806fdf5a0ba381391f4ce4b73bc6e7c974300
cuteviking <pierrenorrbrink@gmail.com>
2018-07-25 13:04:04 +0200

    init and added nuxt

---
+++ nuxt.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'color-by-mouse-pos',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Display a color by the mouse position' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
