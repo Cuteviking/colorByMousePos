ee4206fe6c3b884be6b4c618a670af0c3dcdd6d8
cuteviking <pierrenorrbrink@gmail.com>
2018-07-25 15:11:36 +0200

    the background now change color with the mouse

--- pages/index.vue
+++ pages/index.vue
@@ -1,58 +1,66 @@
 <template>
-  <section class="container">
-    <div>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
+  <section class="background-color" :style="style">
+    <div @mousemove="changeColorOnParent" class="selector-area">
+      
     </div>
   </section>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+import Toolbar from '~/components/Toolbar.vue'
 
 export default {
   components: {
-    AppLogo
+    Toolbar
+  },
+  data() {
+    return {
+      style: {
+        backgroundColor: '#ffffff'
+      },
+      mousePos: {
+        x: 0,
+        y: 0
+      }
+    }
+  },
+  methods: {
+    changeColorOnParent(event) {
+      this.getMousePos(event);
+      this.style.backgroundColor = this.calcColor(this.mousePos);
+      console.log(this.style.backgroundColor);
+    },
+    getMousePos(event) {
+      this.mousePos.x = 1 + Math.floor((event.offsetX / event.target.offsetWidth)*100);
+      this.mousePos.y = 1 + Math.floor((event.offsetY / event.target.offsetHeight)*100);
+    },
+    calcColor(mousePos){
+      //0,0 = 
+      //
+      const h = Math.floor(360 * (mousePos.x / 100));
+      const s = '100%';
+      const l = 100 - mousePos.y + '%';
+
+      return 'hsl(' + h + ',' + s + ',' + l + ')';
+    }
   }
 }
 </script>
 
-<style>
-.container {
+<style lang="scss">
+.background-color {
   min-height: 100vh;
   display: flex;
   justify-content: center;
   align-items: center;
   text-align: center;
-}
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif; /* 1 */
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+  .selector-area {
+    width:100%;
+    min-height: calc(100vh - 40px);
+    border: solid 1px #000;
+    margin: 20px;
+  }
 }
 </style>
 
